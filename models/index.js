ab13ce0a030b39af80be67e6cf234f30f8cf507d
JD Marty <joshua.dmarty@gmail.com>
2021-01-22 18:14:15 -0800

    added default and required values for workout schema

--- models/index.js
+++ models/index.js
@@ -10,25 +10,32 @@ const workoutSchema = new Schema({
   exercises: [
     {
       type: {
-        type: String
+        type: String,
+        required: "Enter a type for this workout"
       },
       name: {
-        type: String
+        type: String,
+        required: "Enter a name for this workout"
       },
       duration: {
-        type: Number
+        type: Number,
+        required: "Enter a duration for this workout"
       },
       weight: {
-        type: Number
+        type: Number,
+        default: 0
       },
       reps: {
-        type: Number
+        type: Number,
+        default: 0
       },
       sets: {
-        type: Number
+        type: Number,
+        default: 0
       },
       distance: {
-        type: Number
+        type: Number,
+        default: 0
       }
     }
   ],
