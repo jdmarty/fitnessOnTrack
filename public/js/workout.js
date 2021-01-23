39dc29f0334a750602615cf5c561701d91ee054c
JD Marty <joshua.dmarty@gmail.com>
2021-01-22 17:45:10 -0800

    added comments to workout script

--- public/js/workout.js
+++ public/js/workout.js
@@ -1,38 +1,49 @@
+// function to start the workout page
 async function initWorkout() {
+  // get the last workout
   const lastWorkout = await API.getLastWorkout();
   console.log("Last workout:", lastWorkout);
+  // if there is a last workout...
   if (lastWorkout) {
+    // set the link to navigate to the last workout page
     document
       .querySelector("a[href='/exercise?']")
       .setAttribute("href", `/exercise?id=${lastWorkout._id}`);
-
+    // create an object to summarize the workout
     const workoutSummary = {
       date: formatDate(lastWorkout.day),
       totalDuration: lastWorkout.totalDuration,
       numExercises: lastWorkout.exercises.length,
       ...tallyExercises(lastWorkout.exercises)
     };
-
+    // render that summary to the DOM
     renderWorkoutSummary(workoutSummary);
+  // If there is not, render a no workout DOM
   } else {
     renderNoWorkoutText()
   }
 }
 
+// function to summarize all exercise data
 function tallyExercises(exercises) {
+  // reduce exercises into an object
   const tallied = exercises.reduce((acc, curr) => {
+    // for resistance exercises, add weight, sets, and reps to accumulator
     if (curr.type === "resistance") {
       acc.totalWeight = (acc.totalWeight || 0) + curr.weight;
       acc.totalSets = (acc.totalSets || 0) + curr.sets;
       acc.totalReps = (acc.totalReps || 0) + curr.reps;
+    // for cardio exercises, add distance to the accumulator
     } else if (curr.type === "cardio") {
       acc.totalDistance = (acc.totalDistance || 0) + curr.distance;
     }
     return acc;
   }, {});
+  // return the summary object
   return tallied;
 }
 
+// function to create a formatted current date
 function formatDate(date) {
   const options = {
     weekday: "long",
@@ -40,13 +51,14 @@ function formatDate(date) {
     month: "long",
     day: "numeric"
   };
-
+  // return the current data
   return new Date(date).toLocaleDateString(options);
 }
 
+// function to render a summary of the current workout to the DOM
 function renderWorkoutSummary(summary) {
   const container = document.querySelector(".workout-stats");
-
+  // Headers for DOM elements
   const workoutKeyMap = {
     date: "Date",
     totalDuration: "Total Workout Duration",
@@ -56,7 +68,7 @@ function renderWorkoutSummary(summary) {
     totalReps: "Total Reps Performed",
     totalDistance: "Total Distance Covered"
   };
-
+  // Update DOM with summary data
   Object.keys(summary).forEach(key => {
     const p = document.createElement("p");
     const strong = document.createElement("strong");
@@ -71,6 +83,7 @@ function renderWorkoutSummary(summary) {
   });
 }
 
+// function to render no workout page
 function renderNoWorkoutText() {
   const container = document.querySelector(".workout-stats");
   const p = document.createElement("p");
@@ -81,4 +94,5 @@ function renderNoWorkoutText() {
   container.appendChild(p);
 }
 
+// initiate workout page
 initWorkout();
